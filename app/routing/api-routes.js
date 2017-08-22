1e7284357c8782b360bcbb650b40057ab950245b
buikevin96 <buikevin96@gmail.com>
2017-08-21 23:45:27 -0400

    Completed Hot Restaurant

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -13,4 +13,28 @@ module.exports = function (app) {
 		//... Display waitingListData
 		res.json(waitingListData);
 	});
+
+	// When post to /api/tables, run this function
+	app.post("/api/tables", function (req, res) {
+		// If less than 5 entries
+		if(tableData.length < 5) {
+			// Send reservation to tableData array
+			tableData.push(req.body);
+			// Boolean value to trigger different message to user
+			res.json(true);
+		} else {
+			// Otherwise send to waitListData array
+			waitingListData.push(req.body);
+			res.json(false);
+		}
+	});
+
+	app.post("/api/clear", function(){
+		// Clear the tableData and waitingListData array
+		tableData = [];
+		waitingListData = [];
+
+		console.log(tableData);
+		console.log(waitingListData);
+	})
 }
